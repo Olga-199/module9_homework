a156dd7de8de4ea8b08f793f12fb0c7eb0ed18bb
Olga-199 <121151860+Olga-199@users.noreply.github.com>
2023-01-25 06:02:09 +0300

    Add files via upload

---
+++ zadanie2.js
@@ -0,0 +1,18 @@
+const person = {
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }
+   
+   const jsonPerson = JSON.stringify(person);
+   
+   console.log(jsonPerson);
